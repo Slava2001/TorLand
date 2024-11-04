24349ffc593d8ea7bf6f5f7a7fcd561c70ef47f8
Slava2001 <slavakaplya20011501@gmail.com>
2024-11-04 12:41:43 +0300

    fix gitignore

---
+++ site/torland/torland_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_worldwraper_free(a: number, b: number): void;
+export function worldwraper_new(a: number): number;
+export function worldwraper_update(a: number): void;
+export function worldwraper_draw(a: number, b: number): void;
+export function compile(a: number, b: number, c: number): void;
+export function decompile(a: number, b: number, c: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
