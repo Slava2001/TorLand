13e4876901931a9c74f2f012de43850ea4ae5bc9
Slava2001 <slavakaplya20011501@gmail.com>
2024-11-15 08:28:24 +0300

    fix

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
 wasm-pack build .\botc\ --target web --release --out-dir=../site/compiler/botc || (echo "Failed to build botc" && exit)
+del .\site\compiler\botc\.gitignore
 wasm-pack build .\torland\ --target web --release --out-dir=../site/torland || (echo "Failed to build torland" && exit)
+del .\site\torland\.gitignore
