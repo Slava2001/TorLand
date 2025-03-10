4656048a7cac20801324453ea5a2460c939ff8f6
nikonru <makaroff.nickita2013@yandex.ru>
2025-03-10 22:48:02 +0300

    add syntax highlights for NiLang

--- site/sections/TorLand/compiler/nilang.js
+++ site/sections/TorLand/compiler/nilang.js
@@ -1,63 +1,34 @@
-const NI_KEYWORDS = {
-    'NI_COMMANDS': {
-        'If': [],
-        'While': [],
-        'Break': [],
-        'Continue': [],
-        'Using': [],
-        'Else': [],
-        'Elif': [],
-        'And': [],
-        'Or': [],
-        'Fun': [],
-        '$': [],
-        'Return': [],
-    },
-    'Dir': {
-        'values': ['front', 'frontright', 'right', 'backright', 'back', 'backleft', 'left', 'frontleft'],
-        'type': "variable"
-    },
-    'Bools': {
-        'values': ['False', 'True'],
-        'type': "variable"
-    },
-    'Types': {
-        'values': ['Int', 'Bool', 'Dir'],
-        'type': "variable"
-    },
+const NI_STATEMENTS = {
+    'KEYWORDS': [
+        'If',
+        'Else',
+        'Elif',
+        'While',
+        'Break',
+        'Continue',
+        'Using',
+        'Alias',
+        'Fun',
+        'Return',
+    ],
+    'Dir': ['front', 'frontRight', 'right', 'backRight', 'back', 'backLeft', 'left', 'frontLeft'],
+    'Bools': ['False', 'True'],
+    'Logic': ['And', 'Or', 'Not'],
+    'Builtin': ['dir', 'bot', 'Split', 'Fork', 'Bite', 'ConsumeSunlight', 'AbsorbMinerals', 'IsEmpty', 'IsSibling', 'IsFriend', 'GetLuminosity', 'GetMineralization', 'Sleep', 'Move', 'Face'],
+    'Types': ['Int', 'Bool', 'Dir']
 };
-const NI_COMMANDS = Object.keys(NI_KEYWORDS.NI_COMMANDS);
-const NI_TYPES = NI_KEYWORDS.Types.values;
-const NI_BOOLS = NI_KEYWORDS.Bools.values;
-const NI_DIRS = NI_KEYWORDS.Dir.values;
 
-function ni_ValParser(stream, state) {
-    if (/^[+-]?[0-9]*$/.test(stream.current())) {
-        return NI_KEYWORDS['Val'].type;
-    }
-    return 'error';
-}
-
-function ni_MemParser(stream, state) {
-    if (/^[[]{1}[0-9]+[]]{1}$/.test(stream.current())) {
-        return NI_KEYWORDS['Mem'].type;
-    }
-    return 'error';
-}
-
-function ni_LableParser(stream, state) {
-    if (/^[A-Za-z_]+[A-Za-z0-9_]*$/.test(stream.current())) {
-        return NI_KEYWORDS['Lable'].type;
-    }
-    return 'error';
-}
+const NI_KEYWORDS = NI_STATEMENTS.KEYWORDS;
+const NI_TYPES = NI_STATEMENTS.Types;
+const NI_BOOLS = NI_STATEMENTS.Bools;
+const NI_LOGIC = NI_STATEMENTS.Logic;
+const NI_BUILTIN = NI_STATEMENTS.Builtin;
+const NI_DIRS = NI_STATEMENTS.Dir;
 
 CodeMirror.defineMode("NiLang", function () {
     return {
         startState: function () {
             return {
-                lables: [],
-                expect_args: []
             };
         },
         token: function (stream, state) {
@@ -69,49 +40,63 @@ CodeMirror.defineMode("NiLang", function () {
                 return "comment";
             }
 
-            if (stream.eatWhile(/[^\s]/)) {
+            if (stream.eatWhile(/[a-zA-Z0-9_]/)) {
                 word = stream.current().trim();
 
-                // // Args
-                // if (state.expect_args.length > 0) {
-                //     exp_ty = state.expect_args.shift();
-                //     if (NI_KEYWORDS[exp_ty].parser) {
-                //         return NI_KEYWORDS[exp_ty].parser(stream, state);
-                //     }
-                //     if (NI_KEYWORDS[exp_ty].values.some(x => x == word)) {
-                //         return NI_KEYWORDS[exp_ty].type;
-                //     }
-                //     return "error ";
-                // }
+                const is_number = new RegExp("\\b[0-9]+\\b", "g");
+                if (word.match(is_number)) {
+                    return "number";
+                }
 
-                // // Lable
-                // if (LABLE_REGEX.test(word)) {
-                //     if (state.lables.includes(word.slice(0, -1))) {
-                //         return "error";
-                //     }
-                //     return "def";
-                // }
+                if (word == "Fun") {
+                    return "def";
+                }
 
-                // NI_COMMANDS
-                if (NI_COMMANDS.includes(word)) {
-                    state.expect_args = [...NI_KEYWORDS.NI_COMMANDS[word]];
+                if (NI_KEYWORDS.includes(word)) {
                     return "keyword";
+                }
+
+                if (NI_TYPES.includes(word)) {
+                    return "type";
+                }
+                
+                if (NI_LOGIC.includes(word)) {
+                    return "operator";
+                }
+
+                if (NI_BOOLS.includes(word) || NI_DIRS.includes(word)) {
+                    return "builtin";
+                }
+
+                const does_it_begin_with_capital_letter = new RegExp("\\b[A-Z].*?\\b", "g")
+                if (word.match(does_it_begin_with_capital_letter)) {
+                    return "string";
                 }
             }
 
-            stream.next();
+            let next = stream.next();
+            switch (next) {
+                case "=":
+                case "+":
+                case "-":
+                case "/":
+                case "*":
+                case "$":
+                case ">":
+                case "<":
+                  return "operator"
+                case ":":
+                    let another_one = stream.peek()
+                    if (another_one == ":") 
+                        {
+                            stream.next()
+                            return "property"
+                        }
+              }
             return null;
         }
     };
 });
-
-function ni_get_lables() {
-    let all_code = nilang_editor.getValue();
-    let words = all_code.split(/\s/);
-    let _lables = words.filter(s => LABLE_REGEX.test(s));
-    let lables = _lables.map(s => s.replace(':', ''));
-    return lables;
-}
 
 function NiLangHint(cm) {
     const cursor = cm.getCursor();
@@ -120,14 +105,16 @@ function NiLangHint(cm) {
     token.type = "error";
     var filtered = [];
     if (word != "") {
-        filtered = NI_COMMANDS.filter(s => s.startsWith(word) && s != word);
-        filtered = filtered.concat(NI_TYPES.filter(s => s.startsWith(word)
+        filtered = NI_KEYWORDS.filter(s => s.toLowerCase().startsWith(word) && s != word);
+        filtered = filtered.concat(NI_TYPES.filter(s => s.toLowerCase().startsWith(word)
             && s != word));
-        filtered = filtered.concat(NI_BOOLS.filter(s => s.startsWith(word)
+        filtered = filtered.concat(NI_BOOLS.filter(s => s.toLowerCase().startsWith(word)
             && s != word));
-        filtered = filtered.concat(NI_DIRS.filter(s => s.startsWith(word)
+        filtered = filtered.concat(NI_DIRS.filter(s => s.toLowerCase().startsWith(word)
             && s != word));
-        filtered = filtered.concat(ni_get_lables().filter(s => s.startsWith(word)
+        filtered = filtered.concat(NI_LOGIC.filter(s => s.toLowerCase().startsWith(word)
+            && s != word));
+        filtered = filtered.concat(NI_BUILTIN.filter(s => s.toLowerCase().startsWith(word)
             && s != word));
     }
 
