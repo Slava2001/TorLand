d120fb2f3f2a15f2d18293e52824096427ae430a
Slava2001 <slavakaplya20011501@gmail.com>
2025-03-25 20:17:42 +0300

    add submodule

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-wasm-pack build .\botc\ --target web --release --out-dir=../site/compiler/botc || (echo "Failed to build botc" && exit)
-del .\site\compiler\botc\.gitignore
-wasm-pack build .\torland\ --target web --release --out-dir=../site/torland || (echo "Failed to build torland" && exit)
-del .\site\torland\.gitignore
+wasm-pack build .\botc\ --target web --release --out-dir=../site/sections/TorLand/compiler/botc || (echo "Failed to build botc" && exit)
+del .\site\sections\TorLand\compiler\botc\.gitignore
+wasm-pack build .\torland\ --target web --release --out-dir=../site/sections/TorLand/simulation/torland || (echo "Failed to build torland" && exit)
+del .\site\sections\TorLand\simulation\torland\.gitignore
